f1452e813b30d04ff32bc24ee9d58ba8e7f9be07
Joseph Prachar <josephprachar@comcast.net>
2015-04-25 10:07:51 -0700

    Wrote background class

--- Java/Background.java
+++ Java/Background.java
@@ -1,7 +1,21 @@
 // Background class
 
-public class Background{
-    public Background(){
+public class Background
+{
+    private String name;
 
+    public Background(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public String entityString()
+    {
+        return "unknown";
     }
 }
