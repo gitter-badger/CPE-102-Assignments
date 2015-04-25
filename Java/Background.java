4da178989d50d95d8f7b9226a576c943259ae057
Joseph Prachar <josephprachar@comcast.net>
2015-04-25 10:16:08 -0700

    Added point class

--- Java/Background.java
+++ Java/Background.java
@@ -1,21 +1,18 @@
 // Background class
 
-public class Background
-{
+public class Background {
+
     private String name;
 
-    public Background(String name)
-    {
+    public Background(String name) {
         this.name = name;
     }
 
-    public String getName()
-    {
+    public String getName() {
         return this.name;
     }
 
-    public String entityString()
-    {
+    public String entityString() {
         return "unknown";
     }
 }
