4a2c54a4e2838c04f15362eb92f66f47fbbef659
Joseph Prachar <josephprachar@comcast.net>
2015-05-04 20:53:57 -0700

    Testing started

---
+++ Java/Testing.java
@@ -0,0 +1,67 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class Testing {
+
+	@Test
+	public void testBackground() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testBlacksmith() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMiner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObstacle() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testOre() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testOreBlob() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPoint() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPositionable() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testQuake() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testVein() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testWorldModel() {
+		fail("Not yet implemented");
+	}
+
+}
