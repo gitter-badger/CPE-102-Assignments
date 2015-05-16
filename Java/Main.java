b3828285da610d3a19504ebe77e947039f959d1f
Joseph Prachar <josephprachar@comcast.net>
2015-05-16 16:18:26 -0700

    Ignore main.java from last commit (I forgot to save before commit)

--- Java/Main.java
+++ Java/Main.java
@@ -2,15 +2,12 @@ import processing.core.*;
 
 public class Main extends PApplet
 {
-	// @Andrew
-	// going to make these public for simplicity
-	// as they are kind of like global variables
-   public final int windowWidth = 640;
-   public final int windowHeight = 480;
-   public final int viewWidth = 20;
-   public final int viewHeight = 15;
-   public final int tileWidth = windowWidth/viewWidth;
-   public final int tileHeight = windowHeight/viewHeight;
+   private final int windowWidth = 640;
+   private final int windowHeight = 480;
+   private final int viewWidth = 20;
+   private final int viewHeight = 15;
+   private final int tileWidth = windowWidth/viewWidth;
+   private final int tileHeight = windowHeight/viewHeight;
 
    private final int worldScale = 2;
 
